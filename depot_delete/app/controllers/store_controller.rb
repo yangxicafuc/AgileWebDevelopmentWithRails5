1d507feb9aeb2fd037a769ab1619b58c6a197db6
yangxicafuc <yangxicafuc@gmail.com>
2021-06-26 07:23:30 +0800

    Import

---
+++ depot_delete/app/controllers/store_controller.rb
@@ -0,0 +1,5 @@
+class StoreController < ApplicationController
+  def index
+	@product = Product.all
+  end
+end
---
+++ depot_delete/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4002cca4a9b99ad4a232219074f6861198b92af7fcb409aa0f95b81e2e15b44bb1b1787492893227f0dd2958ec8a042d5180513f534dd87e7715f8b7dbcc756e'
